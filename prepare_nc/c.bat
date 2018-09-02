21ce34f5b1089cf2ca224e8de2721258573c2faa
Aixi Wang <aixi.wang@hotmail.com>
2018-09-02 22:19:22 +0800

    Add files via upload

---
+++ prepare_nc/c.bat
@@ -0,0 +1,10 @@
+del *.manifest
+del *.obj
+del *.pch
+del *.idb
+del *.nc
+del *.dep
+del *.htm
+
+del dri*.bmp
+
---
+++ prepare_nc/t.bat
@@ -0,0 +1,7 @@
+bmp2nc > help.txt
+
+BMP2NC test.bmp 20000 1000 -500 0  0  3  5 0x000000 0x06 > 1.nc
+
+pause
+
+
